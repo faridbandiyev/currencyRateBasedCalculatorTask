3d529adc2d60a64fda28d8c3b6632e24447e69e7
faridbandiyev <bendiyevf@gmail.com>
2024-05-20 04:14:27 +0400

    Add project files.

---
+++ currencyRateBasedCalculatorTask/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace currencyRateBasedCalculatorTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Array list
+
+            string[] currency = new string[] { "US Dollar", "Russian Ruble", "Turkish Lira" };
+            string[] currencyCode = new string[] { "USD", "RUB", "TRY" };
+            decimal[] exchangeRate = new decimal[] { 1.7000M, 0.0187M, 0.0527M };
+            string[] commands = { "/show-recent-currency-rates", "/find-currency-rate-by-code", "/calculate-amount-by-currency", "/exit" };
+
+            #endregion
+
+            while (true)
+            {
+                Console.WriteLine("Commands list:");
+                int idxCommands = 0;
+
+                while (idxCommands < commands.Length)
+                {
+                    Console.WriteLine(commands[idxCommands]);
+                    idxCommands++;
+                }
+                Console.WriteLine();
+                Console.WriteLine("============================================================");
+                Console.WriteLine();
+                Console.WriteLine("Enter command:");
+                string command = Console.ReadLine();
+
+                if (command == commands[3])
+                {
+                    Console.WriteLine("Exiting the program, goodbye!");
+                    break;
+                }
+                
+                else if (command == commands[0])
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Recent currency rates:");
+                    int idxCurrency = 0;
+
+                    while (idxCurrency < currency.Length)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine(currency[idxCurrency] + "||" + currencyCode[idxCurrency] + "||" + exchangeRate[idxCurrency]);
+                        idxCurrency++;
+                    }
+
+                }
+
+            }
+
+        }
+    }
+}
